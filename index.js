dbc0e3638284a31b566b69c73821fd31b690148a
Josue Lima <josuedsi@gmail.com>
2021-03-22 11:55:57 -0400

    update port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+
+const PORT = process.env.PORT || 5000
  
 app.get('/', function (req, res) {
-  console.log('root path');
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(PORT)
