2783b8db071e4193511dddae56ed3cf72223775d
Josue Lima <josuedsi@gmail.com>
2021-08-13 01:06:23 -0400

    Use axios

--- index.js
+++ index.js
@@ -18,15 +18,21 @@ app.get('/create-chat-session', async function (_, res) {
    * Securely request a new chat session
    */
   const token = jwt.sign({ iss: process.env.WIDGET_ID }, process.env.JWT_SECRET);
+  const body = {
+    customerId: 'currently_logged_user_id'
+  }
 
-  const response = await fetch(`https://widget.sandbox.chat.pega.digital/${process.env.WIDGET_ID}/create`, {
-    method: 'POST',
-    body: JSON.stringify({ customerId: 'currently_logged_user_id' }),
-    headers: { 'Content-Type': 'application/json', 'authorization': 'Bearer ' + token },
-  });
+  const response = await axios.post(`https://widget.sandbox.chat.pega.digital/${process.env.WIDGET_ID}/create`,
+    body,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: 'Bearer ' + token
+      }
+    },
+  ).promise();
 
-  const data = await response.json();
-  res.send(data.sessionId);
+  res.send(response.sessionId);
 });
 
 app.listen(PORT)
