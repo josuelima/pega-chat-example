f7a472c9e302bdcf990132567808b3fad1564d94
Josue Lima <josuedsi@gmail.com>
2021-08-13 00:07:09 -0400

    Update widget

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
 const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
- 
+
 app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.post('/private-data', function (req, res) {
+app.get('/create-chat-session', function (req, res) {
   // Securely send private data to Chat session
   // Any JSON payload can be sent
-  const privateData = { authenticated: true }
   const token = jwt.sign({ iss: req.body.sessionId }, process.env.JWT_SECRET);
 
-  fetch('https://widget.use1dev.chat.pega.digital/private-data', {
-    method: 'POST',
-    body: JSON.stringify(privateData),
-    headers: { 'Content-Type': 'application/json', 'authorization': 'Bearer ' +  token},
-  });
+  // fetch('https://widget.use1dev.chat.pega.digital/private-data', {
+  //   method: 'POST',
+  //   body: JSON.stringify(privateData),
+  //   headers: { 'Content-Type': 'application/json', 'authorization': 'Bearer ' + token },
+  // });
 
-  res.sendStatus(200)
+  // res.sendStatus(200)
+  res.send("session-123")
 });
- 
+
 app.listen(PORT)
